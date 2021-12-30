5bc7c6bf58d89539fe419191d83a29e64e9353f3
alaa.eddin.ksibati.97 <alaa.eddin.ksibati.97@gmail.com>
2021-12-30 10:57:07 +0200

    adding cart

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
   <!-- <div class="container mx-auto">
   </div> -->
 
-  <header class="border-4 border-teal-600 sm:flex sm:justify-between sm:items-center sm:py-3 sm:px-4">
+  <header
+    class="
+      border-4 border-teal-600
+      sm:flex sm:justify-between sm:items-center sm:py-3 sm:px-4
+    "
+  >
     <div class="flex justify-between items-center py-3 px-4 sm:p-0">
       <!-- logo -->
       <div class="w-28">
@@ -159,6 +164,31 @@
         "
       >
         Contact
+      </a>
+      <a
+        href="#"
+        class="
+          block
+          text-neutral-800
+          hover:rounded hover:text-yellow-400 hover:bg-cyan-200
+          font-semibold
+          px-2
+        "
+      >
+        <svg
+          class="w-6 h-6 fill-white stroke-slate-900 hover:stroke-yellow-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          ></path>
+        </svg>
       </a>
     </div>
   </header>
