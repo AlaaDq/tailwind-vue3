6996a41e881c5f6e693b0e5a43b2dae8c49808e2
alaa.eddin.ksibati.97 <alaa.eddin.ksibati.97@gmail.com>
2021-12-30 11:01:37 +0200

    remove border

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@
   <!-- <div class="container mx-auto">
   </div> -->
 
-  <header
-    class="
-      border-4 border-teal-600
-      sm:flex sm:justify-between sm:items-center sm:py-3 sm:px-4
-    "
-  >
+  <header class="sm:flex sm:justify-between sm:items-center sm:py-3 sm:px-4">
     <div class="flex justify-between items-center py-3 px-4 sm:p-0">
       <!-- logo -->
       <div class="w-28">
@@ -164,6 +159,31 @@
         "
       >
         Contact
+      </a>
+      <a
+        href="#"
+        class="
+          block
+          text-neutral-800
+          hover:rounded hover:text-yellow-400 hover:bg-cyan-200
+          font-semibold
+          px-2
+        "
+      >
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
       </a>
       <a
         href="#"
