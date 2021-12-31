5d1e5f0cee3898e744aeae0d04b2910dee33cef3
alaa.eddin.ksibati.97 <alaa.eddin.ksibati.97@gmail.com>
2021-12-31 15:00:56 +0200

    fix aspect ration

--- src/App.vue
+++ src/App.vue
@@ -244,9 +244,9 @@
     </header>
 
     <!-- intro -->
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-4">
       <!-- left col -->
-      <div class="flex flex-col justify-center px-4 md:px-0">
+      <div class="col-start-1 col-end-3 flex flex-col justify-center px-4 md:px-0">
         <div class="text-4xl md:text-6xl font-semibold md:leading-tight">
           Free Home <span class="text-primary">Delivery Within</span> An Hour
         </div>
@@ -316,7 +316,7 @@
                 bg-primary
                 font-semibold
                 text-sm
-                rounded-r-md
+                rounded-r-md rounded-l-none
               "
             >
               search
@@ -326,19 +326,30 @@
       </div>
       <!--  -->
       <!-- right col -->
-      <div class="aspect-w-1 aspect-h-1">
-        <img src="assets/header/Group 21.jpg" class="object-fit" alt="man" />
-      </div>
+        <div class="col-start-3 md:col-start-4 col-end-5">
+          <div class="aspect-w-1 aspect-h-2">
+            <img
+              src="assets/header/Group 21.jpg"
+              class="object-fit"
+              alt="man"
+            />
+          </div>
+        </div>
+
       <!--  -->
     </div>
     <!--  -->
 
-    <section class="h-screen	bg-split-half ">
-
+    <section class="h-screen bg-split-half">
+      <div class="flex flex-col md:w-1/2 px-4 md:px-0">
+        <div class="text-2xl">Featured Categories</div>
+        <p class="text-base mt-3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi libero
+          asperiores in adipisci, cum quam consequuntur et nam beatae? Quaerat
+          tempora non atque voluptate debitis quibusdam alias saepe odio ut.
+        </p>
+      </div>
     </section>
-
-
-
   </div>
 </template>
 
